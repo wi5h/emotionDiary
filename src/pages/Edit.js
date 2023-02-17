f0fe96d37c0092ecaea3a16d4545cb8cbdbd8e4c
wi5h <114387225+wi5h@users.noreply.github.com>
2023-02-17 15:34:25 +0900

    first commit

---
+++ src/pages/Edit.js
@@ -0,0 +1,40 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이 곳은 일기 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: "wi5h" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
